1741a974cfdd1795635952698297bc3e4f8f109a
Sanjay krishna C <sanjaykrishnacofficial@gmail.com>
2024-08-27 01:30:46 +0530

    calendar widget added

--- expenses/forms.py
+++ expenses/forms.py
@@ -5,3 +5,8 @@ class ExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
         fields = ['description', 'amount', 'category', 'datetime']
+
+    datetime = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        label='Date and Time'
+    )
